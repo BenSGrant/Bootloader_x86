ae8ebbe3040e446dbce7944863dbd64ac4bbb6a0
GaryTheBlobfish <83780368+GaryTheBlobfish@users.noreply.github.com>
2021-05-29 11:14:53 +0100

    cleaned up kernel.c

--- bootloader-working/kernel/kernel.c
+++ bootloader-working/kernel/kernel.c
@@ -1,12 +1,12 @@
 void main() {
     /*
-     create a pointer to a character and point it to 
-     the first text cell of video memory (top left of the screen)
+        create a pointer to a character and point it to 
+        the first text cell of video memory (top left of the screen)
     */
-   char* video_memory = (char*) 0xb8000;
-   /*
-    at the address pointed to by the video_memory variable, store
-    the character 'X'
-   */
-   *video_memory = 'X';
+    char* video_memory = (char*) 0xb8000;
+    /*
+        at the address pointed to by the video_memory variable, store
+        the character 'X'
+    */
+    *video_memory = 'X';
 }
