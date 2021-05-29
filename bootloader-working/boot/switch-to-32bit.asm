20f04cc82ff65196b5d5eb390c6e2b9e9772fd12
GaryTheBlobfish <83780368+GaryTheBlobfish@users.noreply.github.com>
2021-05-29 11:12:38 +0100

    cleaned up switch-to-32bit.asm

--- bootloader-working/boot/switch-to-32bit.asm
+++ bootloader-working/boot/switch-to-32bit.asm
@@ -1,11 +1,11 @@
 [bits 16]
 switch_to_32bit: 
-	cli										; 1. disable interrupts
+	cli						; 1. disable interrupts
 	lgdt [gdt_descriptor] 				; 2. load gdt descriptor (gtd.asm)
 	mov eax, cr0
-	or eax, 0x1							; 3. enable 32 bit protected mode
+	or eax, 0x1					; 3. enable 32 bit protected mode
 	mov cr0, eax
-	jmp CODE_SEG:init_32bit		; 4. far jump
+	jmp CODE_SEG:init_32bit				; 4. far jump
 
 [bits 32]
 init_32bit:
@@ -19,4 +19,4 @@ init_32bit:
 	mov ebp, 0x90000				; 6. setup stack
 	mov esp, ebp
 	
-	call BEGIN_32BIT					; 7. go back to mbr.asm
+	call BEGIN_32BIT				; 7. go back to mbr.asm
